dc3251bb18a7b9dea2076adf6e526814f8a91cf2
rumpnizz <markus.timour@live.se>
2015-01-10 22:27:20 +0100

    Implementing Product API controller

---
+++ AngularFiltering.MVC/ApiController/LazyLoaderApiController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using AngularFiltering.DAL;
+
+namespace AngularFiltering.MVC.Controllers
+{
+    public class LazyLoaderApiController : ApiController
+    {
+        private AngularDbContext _ctx;
+        protected AngularDbContext Ctx
+        {
+            get { return _ctx ?? (_ctx = new AngularDbContext()); }
+        }
+    }
+}
